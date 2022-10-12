d7250e0d5e8db32e6d59a7767f399cae586c858d
Vaishali Yasala <vaishu2327@gmail.com>
2022-10-12 13:33:23 -0700

    updated distinct transformation

--- rdd-distinct.py
+++ rdd-distinct.py
@@ -5,7 +5,7 @@
 # 
 # This transformation is used to ensure there are no duplicates in the dataset
 
-# In[2]:
+# In[6]:
 
 
 from pyspark.sql import SparkSession
@@ -28,13 +28,61 @@ print(distinct_rdd.collect())
 
 
 
-# In[6]:
+# In[2]:
 
 
 #Applying the distinct() transformation on a dataset
+#The output prints out only distinct elements.
 str_rdd = spark.sparkContext.parallelize(['hi','John','how','are','you','doing','David how','how', 'coping'])
 
 
 str_rdd = str_rdd.distinct()
 print(str_rdd.collect())
+
+
+# In[3]:
+
+
+#Below, we are reading data from a file on the local machine. 
+
+input_folder_path = "/Users/vaishaliyasala/Desktop/Github/Spark/Exercise_Dependencies/distinct_file.txt"
+
+
+file_overview_rdd = spark.sparkContext.textFile(input_folder_path, 4)
+
+
+print(file_overview_rdd.collect())
+
+
+
+# In[4]:
+
+
+#Use mapPartitions() transformation is applied on each partition of the RDDs. 
+#A custom function is used to split it into inividual elements
+
+
+def tokenize(iterator):
+    mylist = []
+    for words in iterator:
+        mylist = words.split(" ")
+    return mylist
+
+
+rdd_new = file_overview_rdd.mapPartitions(tokenize)
+
+#Look at the element count in this RDD.
+print("rdd_new = ", rdd_new.collect())
+print("number of elements =", len(rdd_new.collect()))
+
+
+# In[5]:
+
+
+#distinct() transformation gives all the words that are unique to the input RDD. 
+
+str_rdd = rdd_new.distinct()
+print("distinct_rdd = ", str_rdd.collect())
+print("number of elements = ", len(str_rdd.collect()))
+
 
